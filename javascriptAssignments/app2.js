cb0df996cbbe84b948883a696687e54a6e3bb27c
bilaliqbal <muhammadbilaliqbal98@gmail.com>
2020-07-10 18:28:45 +0500

    done

---
+++ javascriptAssignments/app2.js
@@ -0,0 +1,45 @@
+    // chapter 21-25 task 1
+    // var first = prompt("Enter your first name:")
+    // var last = prompt("Enter your last name:")
+    // var full = first + last
+    // document.write("Greetings " + full)
+    // chapter 21-25 task 2
+    // var name = prompt("Enter your mobile name name:")
+    // var find = name.length
+    // document.write(find)
+    // document.write("my favorite phone is " + name+"<br>")
+    // document.write("length is " + find)
+     // chapter 21-25 task 3
+    // var name = "pakistani";
+    // document.write("string = "+name+"<br>")
+    // document.write("Index of n "+ name.indexOf('n'))
+      // chapter 21-25 task 4
+    // var name = "hello world";
+    // document.write("string = "+name+"<br>")
+    // document.write("Index of n "+ name.lastIndexOf('l'))
+       // chapter 21-25 task 5
+    //    var name = "pakistani";
+    //    document.write("string = "+name+"<br>")
+    //    document.write("Index of n "+ name.charAt(3))
+        // chapter 21-25 task 6
+    //     var first = prompt("Enter your first name:")
+    // var last = prompt("Enter your last name:")
+    // document.write("Greetings " + first.concat(last))
+         // chapter 21-25 task 7
+        //  var city = "hyderabad";
+        //     document.write("City = "+city+"<br>")
+        //     document.write("After Replacement "+ city.replace('hyderabad','Islamabad'))
+          // chapter 21-25 task 8
+        //   var message = "Ali and Sami are best friends. They play cricket and football together.";
+        //   document.write("Message = "+message+"<br>")
+        //     document.write("After Replacement "+ message.replace(/and/g,'&'))
+           // chapter 21-25 task 9
+           var val = "472";
+           document.write("Message = "+message+"<br>")
+           document.write("Message = "+message+"<br>")
+           document.write("Message = "+message+"<br>")
+           document.write("Message = "+message+"<br>")
+           
+
+
+    
---
+++ stopWatchAssignment/app.js
@@ -0,0 +1,9 @@
+// define variables to hold time values
+var sec= 0;
+var min=0;
+var hours=0;
+
+// start stopwatch function
+function stopwatch(){
+    
+}
