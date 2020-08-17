0283f5227a0856ac5abc849328619c329d56bad6
bilaliqbal <muhammadbilaliqbal98@gmail.com>
2020-08-17 17:42:13 +0500

    todoappwithfirebaseintegration

--- toDoListAssignment/app.js
+++ toDoListAssignment/app.js
@@ -1,3 +1,25 @@
+//function for delete a task
+function deleteTask(e,key){
+    firebase.database().ref('toDo').child(e.id).remove()
+    e.parentNode.remove();
+}
+//function for edit a task
+function editTask(e){
+    var val = prompt("Edit Your Task:",e.parentNode.firstChild.nodeValue)
+    var editToDo = {
+        value : val,
+        key : e.id,
+    }
+    firebase.database().ref('toDo').child(e.id).set(editToDo)
+    e.parentNode.firstChild.nodeValue=val;
+}
+// function for delete all task
+function clearAll(){
+    firebase.database().ref('toDo').remove()
+    list.innerHTML="";
+}
+
+
 // output todays date
 //today's date
 var date = document.getElementById('date');
@@ -6,6 +28,46 @@ var today = new Date();
 date.innerHTML = today.toLocaleDateString("en-US", options);
 
 var list = document.getElementById("list");
+
+    // show data with firebase
+    firebase.database().ref('toDo').on('child_added',function(data){
+        var li = document.createElement('li')
+        var liText = document.createTextNode(data.val().value)
+        li.appendChild(liText)
+        list.appendChild(li)
+        // delete button for task
+        var delTask  = document.createElement("button")
+        var delText = document.createTextNode('Delete')
+        delTask.setAttribute('id',data.val().key)
+        delTask.setAttribute("onclick","deleteTask(this)");
+        delTask.setAttribute("class","btn")
+        delTask.appendChild(delText)
+        li.appendChild(delTask)
+
+        // Edit button for task
+        var editTask  = document.createElement("button")
+        var editText = document.createTextNode('Edit')
+        editTask.setAttribute('id',data.val().key)
+        editTask.setAttribute("onclick","editTask(this)");
+        editTask.setAttribute("class","btn2")
+        editTask.appendChild(editText)
+        li.appendChild(editTask)
+
+        // Edit button icon
+        var editIcon = document.createElement("i");
+        editIcon.setAttribute("class","fa fa-pencil");
+        editTask.appendChild(editIcon);
+
+        // Delete button icon
+        var deleteIcon = document.createElement("i");
+        deleteIcon.setAttribute("class","fa fa-trash");
+        delTask.appendChild(deleteIcon);
+        
+       
+        console.log(list)
+    })
+
+
 //add todo list
 function toDo(){
     //return the alert pop-up if task is empty
@@ -16,49 +78,16 @@ function toDo(){
     }
     // add toDo task in the list
     var to_do = document.getElementById("input");
-    var li = document.createElement('li')
-    var liText = document.createTextNode(to_do.value)
-    li.appendChild(liText)
-    list.appendChild(li)
-    // delete button for task
-    var delTask  = document.createElement("button")
-    var delText = document.createTextNode('Delete')
-    delTask.setAttribute("onclick","deleteTask(this)");
-    delTask.setAttribute("class","btn")
-    delTask.appendChild(delText)
-    li.appendChild(delTask)
 
-    // Edit button for task
-    var editTask  = document.createElement("button")
-    var editText = document.createTextNode('Edit')
-    editTask.setAttribute("onclick","editTask(this)");
-    editTask.setAttribute("class","btn2")
-    editTask.appendChild(editText)
-    li.appendChild(editTask)
+    // insert data on firebase
+    var database = firebase.database().ref('toDo');
+    var key = database.push().key;
 
-    // Edit button icon
-    var editIcon = document.createElement("i");
-    editIcon.setAttribute("class","fa fa-pencil");
-    editTask.appendChild(editIcon);
+    var toDo = {
+        value : to_do.value,
+        key : key,
+    }
+    database.child(key).set(toDo) 
 
-    // Delete button icon
-    var deleteIcon = document.createElement("i");
-    deleteIcon.setAttribute("class","fa fa-trash");
-    delTask.appendChild(deleteIcon);
-    
     to_do.value = "";
-    console.log(list)
-}
-//function for delete a task
-function deleteTask(e){
-    e.parentNode.remove();
-}
-//function for edit a task
-function editTask(e){
-    var val = prompt("Edit Your Task:",e.parentNode.firstChild.nodeValue)
-    e.parentNode.firstChild.nodeValue=val;
-}
-// function for delete all task
-function clearAll(){
-    list.innerHTML="";
 }
